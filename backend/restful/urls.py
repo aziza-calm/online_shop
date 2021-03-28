da3766f25d10dea425b5d97ebcd6662fbaad3ad6
Pavel Arzhaev <pavel.arzhaev@gmail.com>
2021-03-28 21:07:21 +0300

    remove drf

--- backend/restful/urls.py
+++ backend/restful/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from .views import CourierView
 
 
-app_name = "articles"
+app_name = "restful"
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
-    path('couriers', CourierView.as_view()),
-    path('couriers/<int:pk>', CourierView.as_view())
 ]
