47c4f6dfeb7f0caaf32ff801c3ccdb2fea4f20d1
Pavel Arzhaev <pavel.arzhaev@gmail.com>
2021-03-28 20:51:34 +0300

    remove trailing slash, remove excessive swagger

--- backend/restful/urls.py
+++ backend/restful/urls.py
@@ -5,6 +5,6 @@ from .views import CourierView
 app_name = "articles"
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
-    path('couriers/', CourierView.as_view()),
+    path('couriers', CourierView.as_view()),
     path('couriers/<int:pk>', CourierView.as_view())
 ]
